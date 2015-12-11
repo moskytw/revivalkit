0d972c01b938b4333cf36b480489bb91cc8fff24
Mosky <mosky.tw@gmail.com>
2015-12-12 01:32:52 +0800

    The log actually is the most low-level module

--- revival/core.py
+++ revival/core.py
@@ -2,9 +2,9 @@ from __future__ import print_function
 
 from functools import partial
 
+from revival import log
 from revival import before_exit
 from revival import utils
-from revival import log
 
 # py3 style
 OSError = IOError
