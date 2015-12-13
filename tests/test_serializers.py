71418aec6ce22dc88dec8ce0efa021fc21173b71
Tzu-ping Chung <uranusjr@gmail.com>
2015-12-13 16:45:29 +0800

    Fix pickle serializer tests

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -1,44 +1,9 @@
-import collections
 import pickle
 import tempfile
 
 import pytest
 
 from revivalkit import serializers
-
-
-@pytest.fixture
-def text_file(request):
-    f = tempfile.NamedTemporaryFile(mode='w+')
-
-    def finalizer():
-        f.close()
-
-    request.addfinalizer(finalizer)
-    return f
-
-
-def test_json_dump(text_file):
-    serializers.json.dump(
-        collections.OrderedDict([('name', 'mosky'), ('numbers', [19, 42])]),
-        text_file.name,
-    )
-    text_file.flush()
-    assert text_file.read() == '{"name": "mosky", "numbers": [19, 42]}'
-
-
-@pytest.fixture
-def text_file_with_content(request):
-    f = text_file(request)
-    f.write('{"name": "mosky", "numbers": [19, 42]}')
-    f.flush()
-    return f
-
-
-def test_json_load(text_file_with_content):
-    assert serializers.json.load(text_file_with_content.name) == {
-        'name': 'mosky', 'numbers': [19, 42],
-    }
 
 
 class Thing(object):
@@ -67,27 +32,28 @@ def binary_file(request):
     return f
 
 
-def test_pickle_dump(capsys, binary_file, thing):
-    serializers.pickle.dump(thing, binary_file.name)
+def test_highest_pickle_dump(capsys, binary_file, thing):
+    serializers.highest_pickle.dump(thing, binary_file)
     binary_file.flush()
+    binary_file.seek(0)
+
     o = pickle.load(binary_file)
     assert o.a == 3
-
     o.say_hello()
     assert capsys.readouterr() == ('hello!\n', '')
 
 
 @pytest.fixture
-def binary_file_with_content(request):
+def file_with_pickle(request):
     f = binary_file(request)
     pickle.dump(thing(), f)
     f.flush()
+    f.seek(0)
     return f
 
 
-def test_pickle_load(capsys, binary_file_with_content):
-    o = serializers.pickle.load(binary_file_with_content.name)
+def test_highest_pickle_load(capsys, file_with_pickle):
+    o = serializers.highest_pickle.load(file_with_pickle)
     assert o.a == 3
-
     o.say_hello()
     assert capsys.readouterr() == ('hello!\n', '')
