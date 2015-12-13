8cbb9e676d3159f47ca5fbfe2371fa0f1972c5ff
Tzu-ping Chung <uranusjr@gmail.com>
2015-12-14 03:02:05 +0800

    Add flake8 linting job

--- revivalkit/log.py
+++ revivalkit/log.py
@@ -5,5 +5,6 @@ import sys
 to_print_debug = False
 
 def debug(*args, **arg_ds):
-    if not to_print_debug: return
+    if not to_print_debug:
+        return
     print('revivalkit:debug:', *args, file=sys.stderr, **arg_ds)
